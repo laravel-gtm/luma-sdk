afb380e7c5372549817eda812fa57899d2feebe6
Devon Garbalosa <devon@laravel.com>
2026-04-01 20:10:21 -0400

    updates

--- config/luma.php
+++ config/luma.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 return [
-    'base_url' => env('LUMA_BASE_URL', 'https://api.luma.ai'),
+    'base_url' => env('LUMA_BASE_URL', 'https://public-api.luma.com'),
     'token' => env('LUMA_API_TOKEN'),
 ];
